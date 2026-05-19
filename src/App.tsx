1edd8c8642579c56d0894589a124fa58e7c2756b
Dylan Xu <dunyixu@gmail.com>
2026-05-19 11:20:37 +0800

    修复App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
-❯ cat src/App.tsx
 export default function App() {
   return (
     <div>
